65833d16511c0818de919e7b6907a7337ea88232
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-09-16 03:10:55 +0700

    Add 2 boundary layers

---
+++ bsql_maps/administrative_boundary/boundary-census.sql
@@ -0,0 +1,4 @@
+SELECT
+    'boundary-census' AS name, *
+FROM `openstreetmap-public-data-dev.osm_planet.multipolygons`
+WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'boundary' AND tags.value='census')
