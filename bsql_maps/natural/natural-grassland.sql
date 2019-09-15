dce66dade57a012d4162d37b08d0400fe9138127
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-09-16 03:38:42 +0700

    Add some natural layers

---
+++ bsql_maps/natural/natural-grassland.sql
@@ -0,0 +1,4 @@
+SELECT
+    'natural-grassland' AS name, *
+FROM `openstreetmap-public-data-dev.osm_planet.multipolygons`
+WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'natural' AND tags.value='grassland')
