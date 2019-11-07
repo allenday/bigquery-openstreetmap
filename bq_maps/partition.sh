191c7fc83b17e1e844583cd7e068617e47d31e5d
allenday <allenday@allenday.com>
2019-11-07 16:39:57 +0800

    refactor to use array<date>

--- bq_maps/partition.sh
+++ bq_maps/partition.sh
@@ -3,13 +3,13 @@
 
 LAYERS=$(find . -name '*.sql' -exec basename '{}' .sql \; | sort)
 
-echo "CREATE OR REPLACE FUNCTION \`${GCP_PROJECT}.${BQ_DATASET}.layername2partition\` (name STRING)
-  RETURNS DATE
-  AS (CAST(TIMESTAMP_ADD(TIMESTAMP \"1970-01-01\", INTERVAL (CASE"
+echo "CREATE OR REPLACE FUNCTION \`${GCP_PROJECT}.${BQ_DATASET}.get_partitions\` (name STRING)
+  RETURNS ARRAY<DATE>
+  AS (ARRAY<DATE>[CAST(TIMESTAMP_ADD(TIMESTAMP \"1970-01-01\", INTERVAL (CASE"
 N=1
 for LAYER in $LAYERS
 do
     echo "WHEN name = '$LAYER' THEN $N"
     N=$((N+1))
 done
-echo "ELSE -1 END) DAY) AS DATE));"
+echo "ELSE -1 END) DAY) AS DATE)]);"
