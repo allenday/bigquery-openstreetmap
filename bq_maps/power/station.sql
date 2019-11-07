8aaf3ea8edd5a30ea0c3b7dec05a6b75094d4247
allenday <allenday@allenday.com>
2019-11-07 17:51:41 +0800

    fill table name

--- bq_maps/power/station.sql
+++ bq_maps/power/station.sql
@@ -1,6 +1,6 @@
 SELECT
   6410 AS layer_code, 'power' AS layer_class, 'station' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
-FROM `..features`
+FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'power' AND tags.value='generator')
   AND NOT EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE
        (  tags.key = 'generator:source' AND tags.value = 'nuclear' ) --6411
