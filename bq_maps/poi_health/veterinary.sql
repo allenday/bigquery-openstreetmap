7f6f4bb52afc2b350437f94df411265bd49422c7
allenday <allenday@allenday.com>
2019-11-11 16:03:19 +0800

    poi health issue #23

---
+++ bq_maps/poi_health/veterinary.sql
@@ -0,0 +1,4 @@
+SELECT
+  2129 AS layer_code, 'poi_health' AS layer_class, 'veterinary' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+FROM `openstreetmap-public-data-prod.osm_planet.features`
+WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'amenity' AND tags.value='veterinary')
