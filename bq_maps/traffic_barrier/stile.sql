2f645f826256427302ee73196176f932d2411849
allenday <allenday@allenday.com>
2019-11-11 17:40:26 +0800

    partial traffic issue #35

---
+++ bq_maps/traffic_barrier/stile.sql
@@ -0,0 +1,4 @@
+SELECT
+  5214 AS layer_code, 'barrier' AS layer_class, 'stile' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+FROM `openstreetmap-public-data-prod.osm_planet.features`
+WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key IN ('barrier','highway') AND tags.value='stile')
