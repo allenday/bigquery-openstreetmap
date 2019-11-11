9d85aa3686450e11f94061da2b71ccf8c8031d23
allenday <allenday@allenday.com>
2019-11-11 15:25:26 +0800

    transport issue #37

---
+++ bq_maps/transport/airport.sql
@@ -0,0 +1,9 @@
+SELECT
+  5621 AS layer_code, 'transport' AS layer_class, 'airport' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+FROM `openstreetmap-public-data-prod.osm_planet.features`
+WHERE NOT EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE (tags.key = 'type' AND tags.value='airstrip')
+   AND (
+      EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'amenity' AND tags.value='airport')
+      OR
+      EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'aeroway' AND tags.value='aerodrome')
+      )
--- bq_maps/transport/transport-waterway.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-    'transport-waterway' AS name, *
-FROM `openstreetmap-public-data-dev.osm_planet.lines`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'waterway')
