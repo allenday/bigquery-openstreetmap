311deab101e5de8037da53965e9685e68497ba84
allenday <allenday@allenday.com>
2019-11-13 14:47:36 +0800

    query fix issue #59

--- layered_gis/natural/mine.sql
+++ layered_gis/natural/mine.sql
@@ -2,3 +2,7 @@ SELECT
   4131 AS layer_code, 'natural' AS layer_class, 'mine' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'natural' AND tags.value='mine')
+   OR EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'historic' AND tags.value='mine')
+   OR EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'landuse' AND tags.value='mine')
+   OR EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'survey_point' AND tags.value='mine')
+   OR EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'industrial' AND tags.value='mine')
--- layered_gis/natural/natural.sh
+++ layered_gis/natural/natural.sh
@@ -7,7 +7,6 @@ LAYER=(
         "4112:natural=cliff"
         "4113:natural=volcano"
         "4121:natural=tree"
-        "4131:natural=mine"
         "4132:natural=cave_entrance"
         "4141:natural=beach"
 )
@@ -25,3 +24,14 @@ do
 FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = '$K' AND tags.value='$V')" > "$V.sql"
 done
+
+#4131
+#TODO natural=1, historic=6573, man_made=5624, land_use=1051, survey_point=765, industrial=617. request upstream to confirm it's correct or to alter the upstream query
+  echo "SELECT
+  4131 AS layer_code, 'natural' AS layer_class, 'mine' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
+WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'natural' AND tags.value='mine')
+   OR EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'historic' AND tags.value='mine')
+   OR EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'landuse' AND tags.value='mine')
+   OR EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'survey_point' AND tags.value='mine')
+   OR EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'industrial' AND tags.value='mine')" > "mine.sql"
