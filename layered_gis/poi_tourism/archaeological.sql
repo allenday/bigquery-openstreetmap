240f7aab506dd37a8948ecaf8b0fdfb693650fd9
allenday <allenday@allenday.com>
2019-11-13 14:22:15 +0800

    typos

--- layered_gis/poi_tourism/archaeological.sql
+++ layered_gis/poi_tourism/archaeological.sql
@@ -1,4 +1,4 @@
 SELECT
   2733 AS layer_code, 'poi_tourism' AS layer_class, 'archaeological' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'tourism' AND tags.value='archaeological_site')
+WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'historic' AND tags.value='archaeological_site')
