1754be894d4a1bafc8f4afea0f82a692b1392d2a
allenday <allenday@allenday.com>
2019-11-13 14:22:52 +0800

    normalize query

--- layered_gis/places/places.sh
+++ layered_gis/places/places.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+#TODO is it correct to use layer_name as 'buildings'? in the spec it specifies blank (null?)
 echo "SELECT
-    'place' AS name, *
-FROM \`${GCP_PROJECT}.${BQ_DATASET}.points\`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'place')" > "place.sql"
+    1500 AS layer_code, 'buildings' AS layer_class, 'buildings' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+FROM \`${GCP_PROJECT}.${BQ_DATASET}.multipolygons\`
+WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'building'" > buildings.sql
