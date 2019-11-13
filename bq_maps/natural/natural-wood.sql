2f4e010b4e1f2cf1de3eb4fc87160d395732af2b
allenday <allenday@allenday.com>
2019-11-13 12:30:15 +0800

    class name correction

--- bq_maps/natural/natural-wood.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-    'natural-wood' AS name, *
-FROM `openstreetmap-public-data-dev.osm_planet.multipolygons`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'natural' AND tags.value='wood')
