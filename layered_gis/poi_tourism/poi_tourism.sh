a906ba980746f5174ddf4a4943741b0c7b14288d
Allen Day <allenday@allenday.com>
2019-11-14 16:54:48 +0800

    query tweak, tracked in issue #76

--- layered_gis/poi_tourism/poi_tourism.sh
+++ layered_gis/poi_tourism/poi_tourism.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#TODO report bug on 2704, geofabrik documents it as tourism_information which should be tourism=information
 LAYER=( 
         "2721:tourism=attraction"
         "2722:tourism=museum"
