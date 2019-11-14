2ef3d1b0ac9b83ce94e32c397e673851da56e544
Allen Day <allenday@allenday.com>
2019-11-14 16:29:53 +0800

    query tweak, tracked in issues #59

--- layered_gis/poi_public/town_hall.sql
+++ layered_gis/poi_public/town_hall.sql
@@ -1,4 +1,5 @@
 SELECT
   2008 AS layer_code, 'poi_public' AS layer_class, 'town_hall' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'amenity' AND tags.value='town_hall')
+WHERE
+EXISTS(SELECT 1 FROM UNNEST(all_tags) AS tags WHERE tags.key = 'amenity' AND tags.value='townhall')
