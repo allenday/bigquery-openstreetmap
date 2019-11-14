3ba6320ea352713282a2a952c75457208ad9073b
Allen Day <allenday@allenday.com>
2019-11-14 16:51:24 +0800

    query tweak, tracked in issue #67

--- layered_gis/buildings/buildings.sh
+++ layered_gis/buildings/buildings.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#TODO is it correct to use layer_name as 'buildings'? in the spec it specifies blank (null?)
 echo "SELECT
     1500 AS layer_code, 'buildings' AS layer_class, 'buildings' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
 FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
