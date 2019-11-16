8090546ecd638027922b47814599fd1cb011f9eb
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-11-16 19:47:37 +0700

    Update for the recent schema

--- layered_gis/power/station.sql
+++ layered_gis/power/station.sql
@@ -1,5 +1,5 @@
 SELECT
-  6410 AS layer_code, 'power' AS layer_class, 'station' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+  6410 AS layer_code, 'power' AS layer_class, 'station' AS layer_name, feature_type AS gdal_type, COALESCE(osm_id, osm_way_id) AS osm_id, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'power' AND tags.value='generator')
   AND NOT EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE
--- layered_gis/power/station_nuclear.sql
+++ layered_gis/power/station_nuclear.sql
@@ -1,5 +1,5 @@
 SELECT
-  6411 AS layer_code, 'power' AS layer_class, 'station_nuclear' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+  6411 AS layer_code, 'power' AS layer_class, 'station_nuclear' AS layer_name, feature_type AS gdal_type, COALESCE(osm_id, osm_way_id) AS osm_id, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'power' AND tags.value='generator')
   AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'generator:source' AND tags.value='nuclear')
