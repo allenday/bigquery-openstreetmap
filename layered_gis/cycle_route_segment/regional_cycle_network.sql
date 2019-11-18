0d8f4533fa6f3563c08f29973b1362bc82913b19
Allen Day <allenday@allenday.com>
2019-11-18 12:36:53 +0800

    singularize

--- layered_gis/cycle_route_segment/regional_cycle_network.sql
+++ layered_gis/cycle_route_segment/regional_cycle_network.sql
@@ -1,4 +1,4 @@
 SELECT
-  9102 AS layer_code, 'cycle_route_segments' AS layer_class, 'regional_cycle_network' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+  9102 AS layer_code, 'cycle_route_segment' AS layer_class, 'regional_cycle_network' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'rcn' AND tags.value='yes')
