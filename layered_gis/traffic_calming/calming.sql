48784ca71621427fbb41cd4c1f799f5d16be37fe
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-11-20 22:43:01 +0700

    Fix 'traffic_calming' layers for updated 'features' table

--- layered_gis/traffic_calming/calming.sql
+++ layered_gis/traffic_calming/calming.sql
@@ -1,5 +1,5 @@
 SELECT
-  5230 AS layer_code, 'traffic' AS layer_class, 'calming' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+  5230 AS layer_code, 'traffic' AS layer_class, 'calming' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'traffic_calming')
   AND NOT EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'traffic_calming' AND tags.value='hump')
