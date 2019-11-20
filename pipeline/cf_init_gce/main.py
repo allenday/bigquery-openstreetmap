2fbdca8feaee3b2343461fd0556b980ea5e5f613
Allen Day <allenday@allenday.com>
2019-11-20 14:01:39 +0800

    test

--- pipeline/cf_init_gce/main.py
+++ pipeline/cf_init_gce/main.py
@@ -30,44 +30,45 @@ def get_info(instance_name: str):
 
 
 def create_vm():
-    """Creates GCE instance based on predefined parameters"""
-
-    credentials, _project = google.auth.default()
-    service = build('compute', 'v1', credentials=credentials, cache_discovery=False)
-    body = {
-        'name': 'osmprocess',
-        'machineType': f'zones/{GCE_ZONE}/machineTypes/n1-standard-16',
-        'disks': [
-            {
-                'boot': True,
-                'autoDelete': True,
-                'initializeParams': {
-                    'sourceImage': 'projects/debian-cloud/global/images/debian-10-buster-v20190813',
-                    'diskSizeGb': '1000',
-                    'diskType': f'zones/{GCE_ZONE}/diskTypes/pd-ssd'
-                }
-            }
-        ],
-        'networkInterfaces': [{'accessConfigs': [{'name': 'external-nat',
-                                                  'networkTier': 'PREMIUM',
-                                                  'type': 'ONE_TO_ONE_NAT'}],
-                               'network': 'global/networks/default',
-                               }
-                              ],
-        'metadata': {'items': [
-            {
-                'key': 'startup-script-url',
-                'value': SCRIPT_URL,
-            }
-        ]},
-        'serviceAccounts': [{
-            'email': SERVICE_ACCOUNT_EMAIL,
-            'scopes': ['https://www.googleapis.com/auth/cloud-platform']
-        }]
-    }
-    request = service.instances().insert(project=GCP_PROJECT, zone=GCE_ZONE, body=body)
-    response = request.execute()
-    logging.info(response)
+    logging.info('hello world')
+#    """Creates GCE instance based on predefined parameters"""
+#
+#    credentials, _project = google.auth.default()
+#    service = build('compute', 'v1', credentials=credentials, cache_discovery=False)
+#    body = {
+#        'name': 'osmprocess',
+#        'machineType': f'zones/{GCE_ZONE}/machineTypes/n1-standard-16',
+#        'disks': [
+#            {
+#                'boot': True,
+#                'autoDelete': True,
+#                'initializeParams': {
+#                    'sourceImage': 'projects/debian-cloud/global/images/debian-10-buster-v20190813',
+#                    'diskSizeGb': '1000',
+#                    'diskType': f'zones/{GCE_ZONE}/diskTypes/pd-ssd'
+#                }
+#            }
+#        ],
+#        'networkInterfaces': [{'accessConfigs': [{'name': 'external-nat',
+#                                                  'networkTier': 'PREMIUM',
+#                                                  'type': 'ONE_TO_ONE_NAT'}],
+#                               'network': 'global/networks/default',
+#                               }
+#                              ],
+#        'metadata': {'items': [
+#            {
+#                'key': 'startup-script-url',
+#                'value': SCRIPT_URL,
+#            }
+#        ]},
+#        'serviceAccounts': [{
+#            'email': SERVICE_ACCOUNT_EMAIL,
+#            'scopes': ['https://www.googleapis.com/auth/cloud-platform']
+#        }]
+#    }
+#    request = service.instances().insert(project=GCP_PROJECT, zone=GCE_ZONE, body=body)
+#    response = request.execute()
+#    logging.info(response)
 
 
 def main(request=None):
