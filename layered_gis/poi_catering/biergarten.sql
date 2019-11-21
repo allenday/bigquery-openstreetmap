7aaeddf309ec8f654d613dfd4b78101d76a54c5a
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-11-21 14:03:37 +0700

    Fix 'poi_catering' layers for updated 'features' table

--- layered_gis/poi_catering/biergarten.sql
+++ layered_gis/poi_catering/biergarten.sql
@@ -1,4 +1,4 @@
 SELECT
-  2307 AS layer_code, 'poi_catering' AS layer_class, 'biergarten' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+  2307 AS layer_code, 'poi_catering' AS layer_class, 'biergarten' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'amenity' AND tags.value='biergarten')
