7fc2090ab21b66b7192815b34f7d50f433d3ffbf
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-11-21 19:25:39 +0700

    Fix 'barrier' layers for updated 'features' table

--- layered_gis/barrier/barrier.sh
+++ layered_gis/barrier/barrier.sh
@@ -16,7 +16,7 @@ do
   K="${KV%%=*}"
   V="${KV##*=}"
   echo "SELECT
-  $CODE AS layer_code, 'barrier' AS layer_class, '$V' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+  $CODE AS layer_code, 'barrier' AS layer_class, '$V' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
 FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = '$K' AND tags.value='$V')" > "$V.sql"
 done
@@ -24,6 +24,6 @@ done
 #5501
 #barrier=fence, wood_fence, wire_fence
 echo "SELECT
-  5501 AS layer_code, 'barrier' AS layer_class, 'fence' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+  5501 AS layer_code, 'barrier' AS layer_class, 'fence' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
 FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE (tags.key = 'barrier' AND tags.value='fence') OR (tags.key = 'barrier' AND tags.value='wood_fence') OR (tags.key = 'barrier' AND tags.value='wire_fence'))" > "fence.sql"
