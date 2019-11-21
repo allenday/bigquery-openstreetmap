0189be83fdb9c2fa13c4d3c315c854748932e1ec
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-11-21 14:11:48 +0700

    Fix layer_name from 'guest_house' to 'guesthouse'

--- layered_gis/poi_accommodation/guest_house.sql
+++ layered_gis/poi_accommodation/guest_house.sql
@@ -1,4 +1,4 @@
 SELECT
-  2404 AS layer_code, 'poi_accommodation' AS layer_class, 'guest_house' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
+  2404 AS layer_code, 'poi_accommodation' AS layer_class, 'guesthouse' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'tourism' AND tags.value='guest_house')
--- layered_gis/poi_accommodation/poi_accommodation.sh
+++ layered_gis/poi_accommodation/poi_accommodation.sh
@@ -4,7 +4,6 @@ LAYER=(
         "2401:tourism=hotel"
         "2402:tourism=motel"
         "2403:tourism=bed_and_breakfast"
-        "2404:tourism=guest_house"
         "2405:tourism=hostel"
         "2406:tourism=chalet"
         "2421:amenity=shelter"
@@ -26,3 +25,9 @@ do
 FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = '$K' AND tags.value='$V')" > "$V.sql"
 done
+
+#2404
+echo "SELECT
+  2404 AS layer_code, 'poi_accommodation' AS layer_class, 'guesthouse' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
+FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
+WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'tourism' AND tags.value='guest_house')" > "guest_house.sql"
