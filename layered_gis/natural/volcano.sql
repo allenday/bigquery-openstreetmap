9740a3bc59a0878c06988dc74ae579eddbff0afa
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-11-21 15:45:17 +0700

    Fix 'natural' layers for updated 'features' table

--- layered_gis/natural/volcano.sql
+++ layered_gis/natural/volcano.sql
@@ -1,4 +1,4 @@
 SELECT
-  4113 AS layer_code, 'natural' AS layer_class, 'volcano' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+  4113 AS layer_code, 'natural' AS layer_class, 'volcano' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'natural' AND tags.value='volcano')
