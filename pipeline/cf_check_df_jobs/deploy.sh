7f3adf0133fe4f9bc637a4ce67c832ca769a6f88
Allen Day <allenday@allenday.com>
2019-11-21 16:59:33 +0800

    consistent variable use

--- pipeline/cf_check_df_jobs/deploy.sh
+++ pipeline/cf_check_df_jobs/deploy.sh
@@ -6,4 +6,4 @@ gcloud functions deploy check-df-jobs \
 --runtime python37 \
 --timeout 540 \
 --trigger-topic $PS_TOPIC_DF_JOBS \
---set-env-vars=PUBSUB_DF_TOPIC=$PS_TOPIC_DF_JOBS,PUBSUB_BQ_TOPIC=$PS_LAYERS_TOPIC
+--set-env-vars=PUBSUB_DF_TOPIC=$PS_TOPIC_DF_JOBS,PUBSUB_BQ_TOPIC=$PS_TOPIC_LAYERS
