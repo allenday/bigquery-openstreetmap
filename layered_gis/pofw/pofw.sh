99920672af8477bf400a42cc87aa7d2a3c1e4975
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-11-21 15:05:05 +0700

    Enhance generation script for layer 'pofw'

--- layered_gis/pofw/pofw.sh
+++ layered_gis/pofw/pofw.sh
@@ -23,60 +23,30 @@ FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = '$K' AND tags.value='$V')" > "$V.sql"
 done
 
-#3101
-echo "SELECT
-  3101 AS layer_code, 'pofw' AS layer_class, 'christian_anglican' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
+LAYER=( 
+        "3101:denomination=anglican"
+        "3102:denomination=catholic"
+        "3103:denomination=evangelical"
+        "3104:denomination=lutheran"
+        "3105:denomination=methodist"
+        "3106:denomination=orthodox"
+        "3107:denomination=protestant"
+        "3108:denomination=baptist"
+        "3109:denomination=mormon"
+)
+
+for layer in "${LAYER[@]}"
+do
+  CODE="${layer%%:*}"
+  KV="${layer##*:}"
+  K="${KV%%=*}"
+  V="${KV##*=}"
+  echo "SELECT
+  $CODE AS layer_code, 'pofw' AS layer_class, 'christian_$V' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
 FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
-  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='anglican')" > "christian_anglican.sql"
-#3102
-echo "SELECT
-  3102 AS layer_code, 'pofw' AS layer_class, 'christian_catholic' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
-FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
-  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='catholic')" > "christian_catholic.sql"
-#3103
-echo "SELECT
-  3103 AS layer_code, 'pofw' AS layer_class, 'christian_evangelical' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
-FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
-  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='evangelical')" > "christian_evangelical.sql"
-#3104
-echo "SELECT
-  3104 AS layer_code, 'pofw' AS layer_class, 'christian_lutheran' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
-FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
-  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='lutheran')" > "christian_lutheran.sql"
-#3105
-echo "SELECT
-  3105 AS layer_code, 'pofw' AS layer_class, 'christian_methodist' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
-FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
-  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='methodist')" > "christian_methodist.sql"
-#3106
-echo "SELECT
-  3106 AS layer_code, 'pofw' AS layer_class, 'christian_orthodox' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
-FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
-  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='orthodox')" > "christian_orthodox.sql"
-#3107
-echo "SELECT
-  3107 AS layer_code, 'pofw' AS layer_class, 'christian_protestant' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
-FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
-  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='protestant')" > "christian_protestant.sql"
-#3108
-echo "SELECT
-  3108 AS layer_code, 'pofw' AS layer_class, 'christian_baptist' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
-FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
-  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='baptist')" > "christian_baptist.sql"
-#3109
-echo "SELECT
-  3109 AS layer_code, 'pofw' AS layer_class, 'christian_mormon' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
-FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
-  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='mormon')" > "christian_mormon.sql"
+  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = '$K' AND tags.value='$V')" > "christian_$V.sql"
+done
 
 #3301
 echo "SELECT
