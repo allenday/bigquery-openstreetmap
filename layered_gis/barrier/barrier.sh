959cdd5da54a07b6d698d66f993eb9de7e990ea1
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-11-21 19:29:24 +0700

    Simplify 'barrier' layer SQL definition

--- layered_gis/barrier/barrier.sh
+++ layered_gis/barrier/barrier.sh
@@ -26,4 +26,4 @@ done
 echo "SELECT
   5501 AS layer_code, 'barrier' AS layer_class, 'fence' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
 FROM \`${GCP_PROJECT}.${BQ_DATASET}.features\`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE (tags.key = 'barrier' AND tags.value='fence') OR (tags.key = 'barrier' AND tags.value='wood_fence') OR (tags.key = 'barrier' AND tags.value='wire_fence'))" > "fence.sql"
+WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'barrier' AND tags.value IN ('fence','wood_fence','wire_fence'))" > "fence.sql"
