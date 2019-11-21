d2dbf591c57790fb1c244cbe302de80308def327
Alexey Pechnikov <pechnikov@mobigroup.ru>
2019-11-21 14:18:32 +0700

    Fix 'pofw' layers for updated 'features' table

--- layered_gis/pofw/christian_evangelical.sql
+++ layered_gis/pofw/christian_evangelical.sql
@@ -1,5 +1,5 @@
 SELECT
-  3103 AS layer_code, 'pofw' AS layer_class, 'christian_evangelical' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+  3103 AS layer_code, 'pofw' AS layer_class, 'christian_evangelical' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
   AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='evangelical')
--- layered_gis/pofw/muslim_shia.sql
+++ layered_gis/pofw/muslim_shia.sql
@@ -1,5 +1,5 @@
 SELECT
-  3302 AS layer_code, 'pofw' AS layer_class, 'muslim_shia' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
+  3302 AS layer_code, 'pofw' AS layer_class, 'muslim_shia' AS layer_name, feature_type AS gdal_type, osm_id, osm_version, osm_timestamp, all_tags, geometry
 FROM `openstreetmap-public-data-prod.osm_planet.features`
 WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='muslim')
   AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='shia')
