3b141b4e1a173f102c665d691981cafdb6c61b34
Allen Day <allenday@allenday.com>
2019-11-22 13:30:21 +0800

    pass vars, require auth

--- pipeline/deploy-03-check_df.sh
+++ pipeline/deploy-03-check_df.sh
@@ -2,9 +2,10 @@
 
 gcloud functions deploy check-df-$STAGE \
 --project $GCP_PROJECT \
+--no-allow-unauthenticated \
 --entry-point main \
 --runtime python37 \
 --timeout 540 \
 --trigger-topic $PS_TOPIC_DF \
 --source=$SOURCE_ROOT/pipeline/cf_check_df \
---set-env-vars=PS_TOPIC_DF=$PS_TOPIC_DF,PS_TOPIC_LAYERS=$PS_TOPIC_LAYERS
+--set-env-vars=STAGE=$STAGE,PS_TOPIC_DF=$PS_TOPIC_DF,PS_TOPIC_LAYERS=$PS_TOPIC_LAYERS
