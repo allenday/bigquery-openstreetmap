6153422b09959fda0cb2a334d96409f3b935ed0c
Allen Day <allenday@allenday.com>
2019-11-22 10:15:29 +0800

    harmonize vars, code from source repo

--- pipeline/cf_init_df/deploy.sh
+++ pipeline/cf_init_df/deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-gcloud functions deploy init-df \
+gcloud functions deploy init-df-$STAGE \
 --project $GCP_PROJECT \
+--no-allow-unauthenticated \
 --entry-point main \
 --runtime python37 \
 --trigger-resource $GCS_GEOJSON_BUCKET	\
 --trigger-event google.storage.object.finalize \
---set-env-vars=DF_TEMPLATE=$DATAFLOW_TEMPLATE_LOCATION,DF_WORKING_BUCKET=$GCS_BUCKET,BQ_TARGET_DATASET=$BQ_TARGET_DATASET
+--source=$SOURCE_ROOT/pipeline/cf_init_df \
+--set-env-vars=DF_TEMPLATE=$DF_TEMPLATE_LOCATION,DF_WORKING_BUCKET=$GCS_BUCKET,BQ_TARGET_DATASET=$BQ_TARGET_DATASET
