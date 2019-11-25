93db81cfd621a4fa1ad6ef8bef2e63546b89e1fa
Allen Day <allenday@allenday.com>
2019-11-25 16:26:51 +0800

    Minimize partition helper function SQL code

--- layered_gis/partition.sh
+++ layered_gis/partition.sh
@@ -1,31 +1,25 @@
 #!/bin/sh
 # https://cloud.google.com/bigquery/docs/partitioned-tables#comparing_partitioning_options
 
-LAYERS=`find . -type d -depth 1 | xargs basename | sort`
+LAYER_CLASSES=`find . -type d -depth 1 | xargs basename | sort`
 
-echo "CREATE OR REPLACE FUNCTION \`${GCP_PROJECT}.${BQ_SOURCE_DATASET}.gd\`(j INT64)
-RETURNS ARRAY<DATE>
-AS(ARRAY<DATE>[(CAST(TIMESTAMP_ADD(TIMESTAMP'1970-01-01',INTERVAL j DAY)AS DATE))]);"
-echo "CREATE OR REPLACE FUNCTION \`${GCP_PROJECT}.${BQ_SOURCE_DATASET}.gda\`(j INT64,k INT64)
-RETURNS ARRAY<DATE>
-AS(GENERATE_DATE_ARRAY(CAST(TIMESTAMP_ADD(TIMESTAMP'1970-01-01',INTERVAL j DAY)AS DATE),CAST(TIMESTAMP_ADD(TIMESTAMP'1970-01-01',INTERVAL k DAY)AS DATE)));"
-
-echo "CREATE OR REPLACE FUNCTION \`${GCP_PROJECT}.${BQ_SOURCE_DATASET}.layer_partition\`(c STRING)
-RETURNS ARRAY<DATE>
-AS(CASE"
+echo "CREATE OR REPLACE FUNCTION \`${GCP_PROJECT}.${BQ_DATASET}.get_partitions\` (name STRING)
+  RETURNS ARRAY<DATE>
+  AS (CASE"
 N=1
-for LAYER in $LAYERS
+for LAYER_CLASS in $LAYER_CLASSES
 do
   K0=$N
   K1=$N
-  LAYER_CLASSES=`find $LAYER -type f -name '*.sql' -exec basename '{}' .sql \; | sort`
-  for LAYER_CLASS in $LAYER_CLASSES
+  LAYER_NAMES=`find $LAYER_CLASS -type f -name '*.sql' -exec basename '{}' .sql \; | sort`
+  for LAYER_NAME in $LAYER_NAMES
   do
-    #echo "$LAYER/$LAYER_CLASS"
-    echo "WHEN c='$LAYER-$LAYER_CLASS' THEN \`${GCP_PROJECT}.${BQ_SOURCE_DATASET}.gd\`($N)"
+    #echo "$LAYER_CLASS/$LAYER_NAME"
+    echo "WHEN name = '$LAYER_NAME' THEN [DATE_ADD('1970-01-01', INTERVAL $N DAY)]"
     K1=$N
     N=$((N+1))
   done
-  echo "WHEN c='$LAYER' THEN \`${GCP_PROJECT}.${BQ_SOURCE_DATASET}.gda\`($K0,$K1)"
+  echo "WHEN name = '$LAYER_CLASS' THEN GENERATE_DATE_ARRAY(DATE_ADD('1970-01-01', INTERVAL $K0 DAY),
+  DATE_ADD('1970-01-01', INTERVAL $K1 DAY))"
 done
-echo "ELSE ARRAY<DATE>[] END);"
+echo "  ELSE ARRAY<DATE>[] END);"
