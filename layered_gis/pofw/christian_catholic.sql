a9a4eb75f87240a3edf1b963bd509c8147f1bc14
Allen Day <allenday@allenday.com>
2020-04-18 21:04:45 +0800

    merge tags from osm nwr tables

--- layered_gis/pofw/christian_catholic.sql
+++ layered_gis/pofw/christian_catholic.sql
@@ -1,5 +1,22 @@
+
+WITH osm AS (
+  SELECT CAST(id AS STRING) AS id, null AS way_id, all_tags FROM `openstreetmap-public-data-dev.osm_planet.nodes`
+  UNION ALL
+  SELECT CAST(id AS STRING) AS id, CAST(id AS STRING) AS way_id, all_tags FROM `openstreetmap-public-data-dev.osm_planet.ways`
+  UNION ALL
+  SELECT CAST(id AS STRING) AS id, null AS way_id, all_tags FROM `openstreetmap-public-data-dev.osm_planet.relations`
+)
 SELECT
-  3102 AS layer_code, 'pofw' AS layer_class, 'christian_catholic' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
-FROM `openstreetmap-public-data-dev.osm_planet.features`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
-  AND EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='catholic')
+  3102 AS layer_code, 'pofw' AS layer_class, 'christian_catholic' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
+FROM
+  `openstreetmap-public-data-dev.osm_planet.features` AS f, osm
+WHERE EXISTS(SELECT 1 FROM UNNEST(osm.all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='catholic') AND osm.id = f.osm_id
+  AND EXISTS(SELECT 1 FROM UNNEST(osm.all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
+UNION ALL
+SELECT
+  3102 AS layer_code, 'pofw' AS layer_class, 'christian_catholic' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
+FROM
+  `openstreetmap-public-data-dev.osm_planet.features` AS f, osm
+WHERE EXISTS(SELECT 1 FROM UNNEST(osm.all_tags) as tags WHERE tags.key = 'denomination' AND tags.value='catholic') AND osm.way_id = f.osm_way_id
+  AND EXISTS(SELECT 1 FROM UNNEST(osm.all_tags) as tags WHERE tags.key = 'religion' AND tags.value='christian')
+
