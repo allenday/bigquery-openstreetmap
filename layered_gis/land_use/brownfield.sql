028e6ecb70d68c8d975c88b1817cd9fb0ec4f9c0
Allen Day <allenday@allenday.com>
2020-04-18 20:13:59 +0800

    merge tags from osm nwr tables

--- layered_gis/land_use/brownfield.sql
+++ layered_gis/land_use/brownfield.sql
@@ -1,4 +1,20 @@
+
+WITH osm AS (
+  SELECT CAST(id AS STRING) AS id, null AS way_id, all_tags FROM `openstreetmap-public-data-dev.osm_planet.nodes`
+  UNION ALL
+  SELECT CAST(id AS STRING) AS id, CAST(id AS STRING) AS way_id, all_tags FROM `openstreetmap-public-data-dev.osm_planet.ways`
+  UNION ALL
+  SELECT CAST(id AS STRING) AS id, null AS way_id, all_tags FROM `openstreetmap-public-data-dev.osm_planet.relations`
+)
 SELECT
-  7224 AS layer_code, 'land_use' AS layer_class, 'brownfield' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry 
-FROM `openstreetmap-public-data-dev.osm_planet.features`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE tags.key = 'landuse' AND tags.value='brownfield')
+  7224 AS layer_code, 'land_use' AS layer_class, 'brownfield' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
+FROM
+  `openstreetmap-public-data-dev.osm_planet.features` AS f, osm
+WHERE EXISTS(SELECT 1 FROM UNNEST(osm.all_tags) as tags WHERE tags.key = 'landuse' AND tags.value='brownfield') AND osm.id = f.osm_id
+UNION ALL
+SELECT
+  7224 AS layer_code, 'land_use' AS layer_class, 'brownfield' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
+FROM
+  `openstreetmap-public-data-dev.osm_planet.features` AS f, osm
+WHERE EXISTS(SELECT 1 FROM UNNEST(osm.all_tags) as tags WHERE tags.key = 'landuse' AND tags.value='brownfield') AND osm.way_id = f.osm_way_id
+
