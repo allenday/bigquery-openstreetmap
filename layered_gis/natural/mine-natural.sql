1611d125fd5805390428ec815130dbff65865824
Allen Day <allenday@allenday.com>
2020-04-18 20:34:22 +0800

    merge tags from osm nwr tables

---
+++ layered_gis/natural/mine-natural.sql
@@ -0,0 +1,20 @@
+
+WITH osm AS (
+  SELECT CAST(id AS STRING) AS id, null AS way_id, all_tags FROM `openstreetmap-public-data-dev.osm_planet.nodes`
+  UNION ALL
+  SELECT CAST(id AS STRING) AS id, CAST(id AS STRING) AS way_id, all_tags FROM `openstreetmap-public-data-dev.osm_planet.ways`
+  UNION ALL
+  SELECT CAST(id AS STRING) AS id, null AS way_id, all_tags FROM `openstreetmap-public-data-dev.osm_planet.relations`
+)
+SELECT
+  4131 AS layer_code, 'natural' AS layer_class, 'mine' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
+FROM
+  `openstreetmap-public-data-dev.osm_planet.features` AS f, osm
+WHERE EXISTS(SELECT 1 FROM UNNEST(osm.all_tags) as tags WHERE tags.key = 'natural' AND tags.value='mine') AND osm.id = f.osm_id
+UNION ALL
+SELECT
+  4131 AS layer_code, 'natural' AS layer_class, 'mine' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
+FROM
+  `openstreetmap-public-data-dev.osm_planet.features` AS f, osm
+WHERE EXISTS(SELECT 1 FROM UNNEST(osm.all_tags) as tags WHERE tags.key = 'natural' AND tags.value='mine') AND osm.way_id = f.osm_way_id
+
