3a73e2e1d1f7a7fd62de9176a13cb83de9de07da
Allen Day <allenday@allenday.com>
2020-04-18 19:29:53 +0800

    merge tags from osm nwr tables

--- layered_gis/aeroway/aeroway.sh
+++ layered_gis/aeroway/aeroway.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+CLASS=aeroway
 LAYER=( 
         "6701:aeroway=runway"
         "6702:aeroway=taxiway"
@@ -22,13 +23,13 @@ WITH osm AS (
   SELECT CAST(id AS STRING) AS id, null AS way_id, all_tags FROM \`${GCP_PROJECT}.${BQ_SOURCE_DATASET}.relations\`
 )
 SELECT
-  $CODE AS layer_code, 'aeroway' AS layer_class, '$V' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
+  $CODE AS layer_code, '$CLASS' AS layer_class, '$V' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
 FROM
   \`${GCP_PROJECT}.${BQ_SOURCE_DATASET}.features\` AS f, osm
 WHERE EXISTS(SELECT 1 FROM UNNEST(osm.all_tags) as tags WHERE tags.key = '$K' AND tags.value='$V') AND osm.id = f.osm_id
 UNION ALL
 SELECT
-  $CODE AS layer_code, 'aeroway' AS layer_class, '$V' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
+  $CODE AS layer_code, '$CLASS' AS layer_class, '$V' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
 FROM
   \`${GCP_PROJECT}.${BQ_SOURCE_DATASET}.features\` AS f, osm
 WHERE EXISTS(SELECT 1 FROM UNNEST(osm.all_tags) as tags WHERE tags.key = '$K' AND tags.value='$V') AND osm.way_id = f.osm_way_id
