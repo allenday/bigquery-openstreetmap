a6bede7567d63ef67a0f1e30ac215393c600d930
Allen Day <allenday@allenday.com>
2020-04-22 21:00:07 +0800

    remove old queries

--- layered_gis/poi_miscpoi/emergency_phone.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-  2921 AS layer_code, 'poi_miscpoi' AS layer_class, 'emergency_phone' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
-FROM `openstreetmap-public-data-dev.osm_planet.features`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE ((tags.key = 'amenity' AND tags.value='emergency_phone') OR (tags.key='emergency' AND tags.value='phone')))
--- layered_gis/poi_miscpoi/fire_hydrant.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-  2922 AS layer_code, 'poi_miscpoi' AS layer_class, 'fire_hydrant' AS layer_name, feature_type AS gdal_type, osm_id, osm_way_id, osm_timestamp, all_tags, geometry
-FROM `openstreetmap-public-data-dev.osm_planet.features`
-WHERE EXISTS(SELECT 1 FROM UNNEST(all_tags) as tags WHERE ((tags.key = 'amenity' AND tags.value='fire_hydrant') OR (tags.key='emergency' AND tags.value='fire_hydrant')))
