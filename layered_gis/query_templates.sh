31a439a68c1fa5b5a60f201563ba7431f4c0fd73
Allen Day <allenday@allenday.com>
2020-04-24 11:54:45 +0800

    regenerate queries with nulled ids

--- layered_gis/query_templates.sh
+++ layered_gis/query_templates.sh
@@ -7,7 +7,7 @@ WITH osm AS (
   UNION ALL
   SELECT CAST(id AS STRING) AS id, null AS way_id, all_tags FROM \`${GCP_PROJECT}.${BQ_SOURCE_DATASET}.relations\`
 )
-SELECT $CODE AS layer_code, '$CLASS' AS layer_class, '$NAME_PREFIX$N' AS layer_name, f.feature_type AS gdal_type, 
+SELECT $CODE AS layer_code, '$CLASS' AS layer_class, '$NAME_PREFIX$N' AS layer_name, f.feature_type AS gdal_type,
 CASE WHEN f.osm_id="" THEN NULL ELSE f.osm_id END AS osm_id,
 CASE WHEN f.osm_way_id="" THEN NULL ELSE f.osm_way_id END AS osm_way_id,
 f.osm_timestamp, osm.all_tags, f.geometry
--- layered_gis/transport/bus_stop-public_transport.sql
+++ layered_gis/transport/bus_stop-public_transport.sql
@@ -6,7 +6,10 @@ WITH osm AS (
   UNION ALL
   SELECT CAST(id AS STRING) AS id, null AS way_id, all_tags FROM `openstreetmap-public-data-dev.osm_planet.relations`
 )
-SELECT 5621 AS layer_code, 'transport' AS layer_class, 'bus_stop' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
+SELECT 5621 AS layer_code, 'transport' AS layer_class, 'bus_stop' AS layer_name, f.feature_type AS gdal_type,
+CASE WHEN f.osm_id= THEN NULL ELSE f.osm_id END AS osm_id,
+CASE WHEN f.osm_way_id= THEN NULL ELSE f.osm_way_id END AS osm_way_id,
+f.osm_timestamp, osm.all_tags, f.geometry
 FROM `openstreetmap-public-data-dev.osm_planet.features` AS f, osm
 WHERE osm.id = f.osm_id
 
@@ -16,7 +19,10 @@ WHERE osm.id = f.osm_id
 
 UNION ALL
 
-SELECT 5621 AS layer_code, 'transport' AS layer_class, 'bus_stop' AS layer_name, f.feature_type AS gdal_type, f.osm_id, f.osm_way_id, f.osm_timestamp, osm.all_tags, f.geometry
+SELECT 5621 AS layer_code, 'transport' AS layer_class, 'bus_stop' AS layer_name, f.feature_type AS gdal_type,
+CASE WHEN f.osm_id= THEN NULL ELSE f.osm_id END AS osm_id,
+CASE WHEN f.osm_way_id= THEN NULL ELSE f.osm_way_id END AS osm_way_id,
+f.osm_timestamp, osm.all_tags, f.geometry
 FROM `openstreetmap-public-data-dev.osm_planet.features` AS f, osm
 WHERE osm.way_id = f.osm_way_id
 
